035df8b2fa08ccb6ceebdafe41190dba48ee59d7
Marta <mpadilla@unb.ca>
2017-05-20 09:42:48 -0300

    mongoose queries example

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,25 @@
+const {ObjectID}=require('mongodb');
+
+const {mongoose}=require('./../server/db/mongoose');
+const {Todo}=require('./../server/models/todo');
+
+var id = '591f943a69363c84075461fd';
+
+Todo.find({
+  _id: id
+}).then((todos)=>{
+  console.log('Todos',todos);
+});
+
+Todo.findOne({
+  _id: id
+}).then((todo)=>{
+  console.log('Todo',todo);
+});
+
+Todo.findById(id).then((todo)=>{
+  if(!todo){
+    return console.log('Id not found');
+  }
+  console.log('Todo By Id',todo);
+}).catch((e)=>console.log(e));
